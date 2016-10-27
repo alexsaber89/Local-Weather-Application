1ccd5d4587084f89ddcdcf5e89a2c96b4bdbaaaf
Alex Saber <Alex Saber>
2016-10-26 23:47:21 -0500

    added ability to submit request with enter key

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,42 +1,54 @@
 "use strict";
 
-$(".search-bar").show();
-$("#output").hide();
-
-let weatherInfo = (searchedZipCode) => {
-  return new Promise ((resolve,reject) => {
-    $.ajax({
-      method:"GET",
-      url:`http://api.openweathermap.org/data/2.5/weather?zip=${searchedZipCode},us&APPID=349ecc5fc8a79b57d436f8da19c6af12`
-    }).then((response)=>{
-      resolve(response);
-    });
-  });
-};
-
-function validateUserZipCode(userInput) {
-  if(userInput.length === 5 && $.isNumeric(userInput)) {
-    return true;
-  } else {
-    displayWinnerModal("Invalid zip code");
-    return false;
-  }
-}
-
-function displayWinnerModal(message) {
-  $("#myModal").modal('show');
-  $("#myModalLabel").html(message);
-}
-
 $(document).ready(function() {
-  console.log("document ready");
-  $("#search-button").on("click",()=>{
+
+  $(".search-bar").show();
+  $("#output").hide();
+
+  let weatherInfo = (searchedZipCode) => {
+    return new Promise ((resolve,reject) => {
+      $.ajax({
+        method:"GET",
+        url:`http://api.openweathermap.org/data/2.5/weather?zip=${searchedZipCode},us&units=imperial&APPID=349ecc5fc8a79b57d436f8da19c6af12`
+      }).then((response)=>{
+        resolve(response);
+      });
+    });
+  };
+
+  function enterKeyValidation(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+      submitAJAX();
+    }
+  }
+
+  function submitAJAX() {
     let searchedZipCode = $("#weather-search").val();
     if(validateUserZipCode(searchedZipCode)) {
-      console.log("searched zip code: ",searchedZipCode);
       weatherInfo(searchedZipCode).then((returnedWeatherInfo)=>{
         console.log(returnedWeatherInfo);
+        $("#myModal").modal('show');
+        $("#myModalLabel").html("Temperature: " + Math.round(returnedWeatherInfo.main.temp));
+        $("#myModalLabel").append("<br />Temperature: " + Math.round(returnedWeatherInfo.main.temp));
       });
     }
-  });
+  }
+
+  function validateUserZipCode(userInput) {
+    if(userInput.length === 5 && $.isNumeric(userInput)) {
+      return true;
+    } else {
+      displayWinnerModal("Invalid zip code");
+      return false;
+    }
+  }
+
+  function displayWinnerModal(message) {
+    $("#myModal").modal('show');
+    $("#myModalLabel").html(message);
+  }
+
+  $("#search-button").on("click",submitAJAX);
+  $("#weather-search").on("keyup",enterKeyValidation);
 });
