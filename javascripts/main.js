c692c956e73d0ceb33b36f6936c685eb6cb4328f
Alex Saber <Alex Saber>
2016-10-29 01:37:49 -0500

    able to console.log 3 or 7 day forecast, based on user choice

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,31 +16,60 @@ $(document).ready(function() {
     });
   };
 
+  let weatherForecast = (url) => {
+    return new Promise ((resolve,reject) => {
+      $.ajax({
+        method:"GET",
+        url:`${url}`
+      }).then((response)=>{
+        resolve(response);
+      });
+    });
+  };
+
   function enterKeyValidation(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
-      submitAJAX();
+      returnZipCodeWeather();
     }
   }
 
-  function submitAJAX() {
+  function returnZipCodeWeather() {
     let searchedZipCode = $("#weather-search").val();
+    let multiDayAjaxUrl = "";
     $("#weather-search").val("");
     if(validateUserZipCode(searchedZipCode)) {
       weatherInfo(searchedZipCode).then((returnedWeatherInfo)=>{
         console.log(returnedWeatherInfo);
+        let cityName = returnedWeatherInfo.name;
         $("#myModal").modal('show');
         $("#myModalLabel").html("Searched Zip Code: " + searchedZipCode);
         $("#myModalLabel").append("<br />Temperature: " + Math.round(returnedWeatherInfo.main.temp) + " degrees fahrenheit");
         $("#myModalLabel").append(`<br />Conditions: ${returnedWeatherInfo.weather[0].description}`);
-        $("#myModalLabel").append(`<br />Pressure: ${returnedWeatherInfo.main.pressure} mbar`);
+        $("#myModalLabel").append(`<br />Air Pressure: ${returnedWeatherInfo.main.pressure} mbar`);
         $("#myModalLabel").append(`<br />Wind Speed: ${returnedWeatherInfo.wind.speed} mph`);
-        $("#myModalLabel").append(`<br />Wind Speed: ${returnedWeatherInfo.wind.speed} mph`);
-        $("#myModalLabel").append(`<br /><div id="btn_container"><button type="button" id="3_day_btn" class="btn btn-secondary" data-dismiss="modal">3 day forecast</button><button type="button" id="7_day_btn" class="btn btn-secondary" data-dismiss="modal">7 day forecast</button></div>`);
+        $("#myModalLabel").append(`<br /><div id="btn_container"><button type="button" id="3_day_btn" class="btn btn-secondary">3 day forecast</button><button type="button" id="7_day_btn" class="btn btn-secondary">7 day forecast</button></div>`);
         $("#3_day_btn").addClass("centered");
+        $("#3_day_btn").on("click",function() {
+          multiDayAjaxUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&cnt=3&APPID=349ecc5fc8a79b57d436f8da19c6af12`;
+          console.log("multiDayAjaxUrl: ",multiDayAjaxUrl);
+          displayRequestedForecast(multiDayAjaxUrl);
+        });
         $("#7_day_btn").addClass("centered");
+        $("#7_day_btn").on("click",function() {
+          multiDayAjaxUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&cnt=7&APPID=349ecc5fc8a79b57d436f8da19c6af12`;
+          console.log("multiDayAjaxUrl: ",multiDayAjaxUrl);
+          displayRequestedForecast(multiDayAjaxUrl);
+        });
       });
     }
+  }
+
+  function displayRequestedForecast(url) {
+    weatherForecast(url).then((returnedWeatherInfo)=>{
+      console.log(returnedWeatherInfo);
+      $("#myModalLabel").append(returnedWeatherInfo);
+    });
   }
 
   function validateUserZipCode(userInput) {
@@ -57,6 +86,6 @@ $(document).ready(function() {
     $("#myModalLabel").html(message);
   }
 
-  $("#search-button").on("click",submitAJAX);
+  $("#search-button").on("click",returnZipCodeWeather);
   $("#weather-search").on("keyup",enterKeyValidation);
 });
